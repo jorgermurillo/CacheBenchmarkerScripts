ccb4b56eeddfef6105aca3831bdf9dde500cfeff
jorge <jorgermurillo@gmail.com>
2018-06-23 12:39:57 -0500

    Cleaned up Benchmarker_run.sh and kill_local.sh

--- Benchmarker_run.sh
+++ Benchmarker_run.sh
@@ -10,10 +10,8 @@ RECORDCOUNT=$6
 
 cd ~/$DIRECTORY 
 pwd
-#nohup  ./bin/$BENCHMARK run redis -P ~/workloads/workloada -p redis.host=$HOST -p redis.port=$PORT  </dev/null > /tmp/redis_test/benchfile$INSTANCE 2>&1  &  
-echo WHAT
+
 echo $INSTANCE
-#echo 'nohup  ./bin/$BENCHMARK run redis -P workloads/workload-replayScheduler_Yahoo -p redis.host=$HOST -p redis.port=$PORT  </dev/null > ~/tmp/redis_test/benchfile$INSTANCE 2>&1  &'
 nohup  ./bin/$BENCHMARK run redis -P workloads/workload-replayScheduler_Resizing -p "redis.host=$HOST" -p "redis.port=$PORT"  -p operationcount=$RECORDCOUNT  </dev/null > ~/tmp/redis_test/benchfile$INSTANCE 2>&1  &  
 
 
